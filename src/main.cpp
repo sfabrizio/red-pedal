c68e032300938d1fa0a403c46208a0a43131094c
Samuel Fabrizio <samuelfabrizio@gmail.com>
2022-06-23 16:56:39 +0200

    [ADD] initial working code
         - v1

---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#include "Keyboard.h"
+
+#define BTN_1_PIN 2
+#define BTN_2_PIN 3
+#define BTN_3_PIN 4
+#define BTN_4_PIN 5
+
+#define LED_1_PIN 21
+#define LED_2_PIN 20
+#define LED_3_PIN 19
+#define LED_4_PIN 18
+
+static void onBtnPress(char key, uint8_t ledNumber)
+{   
+    digitalWrite(ledNumber, LOW);
+    Keyboard.press(key);
+    Keyboard.releaseAll(); // for just send once press
+    delay(500); // led turn off & on time
+    digitalWrite(ledNumber, HIGH);
+}
+
+
+static void setAllLeds(uint8_t state)
+{   
+    digitalWrite(LED_1_PIN, state);
+    digitalWrite(LED_2_PIN, state);
+    digitalWrite(LED_3_PIN, state);
+    digitalWrite(LED_4_PIN, state);
+}
+
+void setup(void)
+{
+    Keyboard.begin();
+    pinMode(BTN_1_PIN, INPUT);
+    pinMode(BTN_2_PIN, INPUT);
+    pinMode(BTN_3_PIN, INPUT);
+    pinMode(BTN_4_PIN, INPUT);
+
+    pinMode(LED_1_PIN, OUTPUT);
+    pinMode(LED_2_PIN, OUTPUT);
+    pinMode(LED_3_PIN, OUTPUT);
+    pinMode(LED_4_PIN, OUTPUT);
+    setAllLeds(HIGH);
+}
+
+void loop(void)
+{   
+    static uint8_t btn1StateLast = 0;
+    static uint8_t btn2StateLast = 0;
+    static uint8_t btn3StateLast = 0;
+    static uint8_t btn4StateLast = 0;
+    uint8_t btn1State;
+    uint8_t btn2State;
+    uint8_t btn3State;
+    uint8_t btn4State;
+
+    btn1State = digitalRead(BTN_1_PIN);
+    btn2State = digitalRead(BTN_2_PIN);
+    btn3State = digitalRead(BTN_3_PIN);
+    btn4State = digitalRead(BTN_4_PIN);
+
+    if (btn1State != btn1StateLast)
+    {
+        btn1StateLast = btn1State;
+        onBtnPress('1', LED_1_PIN);
+    }
+    if (btn2State != btn2StateLast)
+    {
+        btn2StateLast = btn2State;
+        onBtnPress('2', LED_2_PIN);
+    }
+    if (btn3State != btn3StateLast)
+    {
+        btn3StateLast = btn3State;
+        onBtnPress('3', LED_3_PIN);
+    }
+    if (btn4State != btn4StateLast)
+    {
+        btn4StateLast = btn4State;
+        onBtnPress('4', LED_4_PIN);
+    }
+    delay(50);
+}
